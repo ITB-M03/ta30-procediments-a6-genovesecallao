a40460574c1d95a6db6a1d6f0f62c4e11eca3922
catalina.genovese.7e8 <catalina.genovese.7e8@ibt.cat>
2024-12-09 12:50:08 +0100

    EX1.1

--- src/main/kotlin/controllers/Main.kt
+++ src/main/kotlin/controllers/Main.kt
@@ -1,5 +1,37 @@
 package org.example.controllers
+import java.util.*
 
+data class Punt (
+    var x:Float,
+    var y:Float
+)
 fun main() {
-    //TODO
+    val sc: Scanner = obrimScanner().useLocale(Locale.US)
+
+    // Crear un punto solicitando las coordenadas al usuario
+    val punt = Punt (
+     x= definimNombres(sc, "Inserta un num en el eix de les X: "),
+     y= definimNombres(sc, "Inserta un'altre num en el eix de les X: ")
+    )
+
+    mostraPunt(punt.x, punt.y)
 }
+
+fun obrimScanner(): Scanner {
+    val sc: Scanner = Scanner(System.`in`)
+    return sc
+}
+
+fun definimNombres (sc:Scanner, msg:String) : Float {
+    print(msg)
+    var num:Float
+    num=sc.nextFloat()
+
+    return num
+}
+
+fun mostraPunt(x:Float, y:Float) {
+
+    println("(${String.format(Locale.US, "%.6f", x)}, ${String.format(Locale.US, "%.6f", y)})")
+}
+
