0e31f90c1459ef6816fdbcfde54adaea52678ed2
catalina.genovese.7e8 <catalina.genovese.7e8@ibt.cat>
2024-12-09 13:04:25 +0100

    EX1.2

--- src/main/kotlin/controllers/Main.kt
+++ src/main/kotlin/controllers/Main.kt
@@ -11,12 +11,22 @@ fun main() {
     // Crear un punto solicitando las coordenadas al usuario
     val punt = Punt (
      x= definimNombres(sc, "Inserta un num en el eix de les X: "),
-     y= definimNombres(sc, "Inserta un'altre num en el eix de les X: ")
+     y= definimNombres(sc, "Inserta un'altre num en el eix de les Y: ")
     )
 
+
+    mostraPunt(punt.x, punt.y)
+
+    //demanem num
+    var a= definimNombres(sc, "Inserta un num")
+    var b= definimNombres(sc, "Inserta un num")
+
+    // Realizar la translación
+    OperacioTranslacio(punt, a, b)
+
+    // actaulizamos valor
     mostraPunt(punt.x, punt.y)
 }
-
 fun obrimScanner(): Scanner {
     val sc: Scanner = Scanner(System.`in`)
     return sc
@@ -35,3 +45,9 @@ fun mostraPunt(x:Float, y:Float) {
     println("(${String.format(Locale.US, "%.6f", x)}, ${String.format(Locale.US, "%.6f", y)})")
 }
 
+
+fun OperacioTranslacio(punt: Punt, a: Float, b: Float) {
+    punt.x += a
+    punt.y += b
+
+}
